c86ccd7c405a699295ea59139425e3368b8fd27a
Roberto Achacoso <bertoa@gmail.com>
2014-06-04 13:10:40 -0700

    Objectify

--- app.rb
+++ app.rb
@@ -1,33 +1,35 @@
 require 'sinatra'
 require 'rest_client'
 
+class Storyset
+	def get_set(json_file, subset)
+		
+		resp = RestClient.get(json_file)
+		doc = JSON.parse(resp)
+		set = doc[subset]
+		return set
 
-def get_set(json_file, subset)
-	
-	resp = RestClient.get(json_file)
-	doc = JSON.parse(resp)
-	set = doc[subset]
-	return set
-
+	end
 end
 
-def get_list(type, set)
+class Filters
+	def get_list(type, set)
 
-	list=[]
-	set.each do |story|
-		list << story[type]
+		list=[]
+		set.each do |story|
+			list << story[type]
+		end
+		return list
 	end
-	return list
-
 end
 
 get '/' do 
 
-	all_stories = get_set('http://mashable.com/stories.json', 'new')
+	all_stories = Storyset.new.get_set('http://mashable.com/stories.json', 'new')
 
 	#for filter menu
-	@channels = get_list('channel', all_stories)
-	@authors = get_list('author', all_stories)
+	@channels = Filters.new.get_list('channel', all_stories)
+	@authors = Filters.new.get_list('author', all_stories)
 
 	#set to render
 	@stories = all_stories
@@ -42,11 +44,11 @@ get '/filter' do
 
 	if params[:filter] && params[:type]
 
-		all_stories = get_set('http://mashable.com/stories.json', 'new')
+		all_stories = Storyset.new.get_set('http://mashable.com/stories.json', 'new')
 
 		#for filter menu
-		@channels = get_list('channel', all_stories)
-		@authors = get_list('author', all_stories)
+		@channels = Filters.new.get_list('channel', all_stories)
+		@authors = Filters.new.get_list('author', all_stories)
 
 		@stories = all_stories.select { |v| v[params[:type]] == params[:filter] }
 
