2be1cd5d80dd0c2792a328c153fee8ebcd4c03cd
Roberto Achacoso <bertoa@gmail.com>
2014-06-04 20:01:03 -0700

    Add twitter and change UI

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'rest_client'
+gem 'twitter'
--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+
+set :server, 'webrick'
+
 require 'rest_client'
+require 'twitter'
+
+def twitter_get 
+
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key    = "9fOe6Z86v8wMT7GrhOepz5GXi"
+	  config.consumer_secret = "eM8iGayXIT0eKwfYd0udXCeXkmFSBLfyof2c6m8Rg0X1RiQFXz"
+	end
+	return client
+
+end
 
 class Storyset
 	def get_set(json_file, subset)
@@ -19,13 +33,19 @@ class Filters
 		set.each do |story|
 			list << story[type]
 		end
-		return list
+		return list.uniq.sort
 	end
 end
 
 get '/' do 
 
-	all_stories = Storyset.new.get_set('http://mashable.com/stories.json', 'new')
+  if params[:set]
+		@mashset = params[:set]
+  else
+  	@mashset = 'new'
+  end
+
+	all_stories = Storyset.new.get_set('http://mashable.com/stories.json', @mashset)
 
 	#for filter menu
 	@channels = Filters.new.get_list('channel', all_stories)
@@ -42,15 +62,23 @@ end
 
 get '/filter' do
 
-	if params[:filter] && params[:type]
+	if params[:filter] && params[:type] && params[:set]
 
-		all_stories = Storyset.new.get_set('http://mashable.com/stories.json', 'new')
+		all_stories = Storyset.new.get_set('http://mashable.com/stories.json', params[:set])
 
 		#for filter menu
-		@channels = Filters.new.get_list('channel', all_stories)
-		@authors = Filters.new.get_list('author', all_stories)
+		if params[:type] == 'channel'
+			@channels = Filters.new.get_list('channel', all_stories)
+		end
+		if params[:type] == 'author'
+			@authors = Filters.new.get_list('author', all_stories)
+		end
 
 		@stories = all_stories.select { |v| v[params[:type]] == params[:filter] }
+
+		# pass back so can set active buttons in UI
+		@filter = params[:filter]
+		@mashset = params[:set]
 
 	else
 		redirect '/'
@@ -61,6 +89,22 @@ get '/filter' do
 end
 
 
+get '/tweets' do
 
+	if params[:filter] && params[:type] && params[:set]
+		all_stories = Storyset.new.get_set('http://mashable.com/stories.json', params[:set])
+
+		@client = twitter_get
+
+		@stories = all_stories.select { |v| v[params[:type]] == params[:filter] }
+		@mashset = params[:set]
+
+	else
+		redirect '/'
+	end
+
+	erb :tweets
+
+end
 
 
